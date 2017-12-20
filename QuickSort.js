089e1c0b3575a70310bef7b39b65beb7a7da3d39
Andrew Kosach <kosach.a@gmail.com>
2017-12-21 01:34:40 +0200

    Added random basic element

--- QuickSort.js
+++ QuickSort.js
@@ -1,8 +1,9 @@
-
+//Quick sort algorithm O(n * log n)
 const quickSort = (array) =>{
+  const selfRandom = (min, max) =>  Math.floor(Math.random() * (max - min + 1)) + min;
   if(array.length < 2) return array;
   else{
-    const pivot = array[0];
+    const pivot = array[selfRandom(0, array.length-1)];
     let less = array.filter(item => item < pivot);
     let greater = array.filter(item => item > pivot);
     return  quickSort(less).concat( pivot, quickSort(greater));
